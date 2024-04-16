c7e41635394f81abe9a1b203eb392d5a6d707be7
Harsh Vijay <72139720+harshvijay63@users.noreply.github.com>
2024-04-16 11:10:25 +0530

    Update Connect.jsx

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -23,7 +23,8 @@ const Connect = () => {
             <div className="hp-s51">
               <i class="fa-brands fa-instagram"></i>
               <a href="/">@kunal_Enterprise</a>
-              <p>FOLLOW US ON INSTAGRAM</p>
+                div className="follow">
+              <p>FOLLOW US ON INSTAGRAM</p> </div>
             </div>
             <div className="hp-s52">
               <div className="hp-s53">
